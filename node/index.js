d189518a388ca9eecd798b4f1af81ad4a287eebf
Stephen Rimbakusumo <Taurvi@users.noreply.github.com>
2016-02-23 11:42:50 -0800

    Update index.js

--- node/index.js
+++ node/index.js
@@ -20,7 +20,7 @@ http.listen(3000, function(){
     serverMsg('Server is now listening on *:3000');
 });
 
-var database = new Firebase("https://wasmun-register.firebaseio.com/");
+var database = new Firebase("");
 
 io.sockets.on('connection', function(socket){
     serverMsg('  User has connected: ' + socket.id);
